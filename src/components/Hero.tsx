a81fa217b9d1fadcae9200cb8f6b28db74ad0ecb
Matthew <matthewleemccane@gmail.com>
2026-05-30 23:33:55 -0500

    Updated hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -16,12 +16,13 @@ export default function Hero() {
                 </h1>
                 <div className="flex flex-col gap-6 font-mono text-sm leading-loose text-text">
                     <p className="font-mono text-lg leading-relaxed text-text  mb-5">
-                            I am currently building and learning @ Arizona State University, while earning a Bachelors of Science in Software Engineering. I have interests in Distributed Systems,
+                            I am currently studying and building my skills @ Arizona State University, pursuing a Bachelors of Science in Software Engineering. My interests lie in Distributed Systems,
                         Infrastructure, Architecture and AI. </p>
-                    <p className="font-mono text-lg leading-relaxed text-text mb-10">   I will also complete a Masters Degree in Computer Science, and long term, I plan to complete my PhD in Philosophy.
-                        When I am not on my computer, I enjoy reading books, exercising, watching CS2, meeting new people, and learning new things!
+                    <p className="font-mono text-lg leading-relaxed text-text mb-10">   I plan to complete a Master's Degree in Computer Science and, in the long term, aim to earn a PhD in Philosophy.
+                        Outside of my academic pursuits, I enjoy reading, exercising, watching CS2, meeting new people, and learning new things!
                     </p>
                 </div>
+
                 <div className="flex gap-4">
                     <Link href="#work" className="bg-black text-white font-mono text-xs tracking-widest uppercase px-6 py-3 rounded-lg hover:opacity-80 transition-colors duration-200">View My Work</Link>
                     <Link href="mailto:mmccane@asu.edu" className="border border-black font-mono text-xs tracking-widest uppercase px-6 py-3 rounded-lg hover:bg-black hover:text-white transition-colors duration-200">Get In Touch!</Link>
